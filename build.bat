b678ba79d6e57c181cefc68899588a0817c3cca2
Rusky <91284372+RuskyDev@users.noreply.github.com>
2024-10-25 16:39:01 +0500

    ⚙️ Update build.bat

--- build.bat
+++ build.bat
@@ -1,3 +1,4 @@
-set version=1.0
+set version=1.1
 pyinstaller --onefile src/main.py --windowed --hidden-import plyer.platforms.win.notification --name=KeyboardSounds-v%VERSION% --icon=src/icon.ico
-@pause>nul
+echo.
+pause
